61ea0c7e6637d4f6025372cac03730e1777aa546
Conner Gleason <connereg@optonline.net>
2022-07-13 12:29:32 -0400

    Finished basics of lab

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         // your code here
+        String firstString = "This is the first string to log";
+        Logger logger = Logger.getInstance();
+
+        logger.log(firstString);
+
+        String secondString = "This is the Second, it will overwrite the Original message";
+
+        logger.log(secondString);
+
     }
 }
